8855b3d6f9b39bd4459b1a00023577d8052dd881
unknown <ajaykumarr3024@gmail.com>
2020-03-20 12:27:30 +0530

    initial commit

---
+++ Processing.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pickle
+import cv2
+from os import listdir
+from sklearn.preprocessing import LabelBinarizer
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+EPOCHS = 25
+INIT_LR = 1e-3
+BS = 32
+default_image_size = tuple((256, 256))
+image_size = 0
+directory_root = cv2.imread("C:/Users/HP/Desktop/pbl.jpg")
+width=256
+height=256
+depth=3
+#Function to convert images to array
+def convert_image_to_array(image_dir):
+    try:
+        image = cv2.imread(image_dir)
+        if image is not None :
+            image = cv2.resize(image, default_image_size)   
+            return img_to_array(image)
+        else :
+            return np.array([])
+    except Exception as e:
+        print(f"Error : {e}")
+        return None
+
+#Fetch images from directory
+image_list, label_list = [], []
+try:
+    print("[INFO] Loading images ...")
+    root_dir = listdir(directory_root)
+    for directory in root_dir :
+        # remove .DS_Store from list
+        if directory == ".DS_Store" :
+            root_dir.remove(directory)
+
+    for plant_folder in root_dir :
+        plant_disease_folder_list = listdir(f"{directory_root}/{plant_folder}")
+        
+        for disease_folder in plant_disease_folder_list :
+            # remove .DS_Store from list
+            if disease_folder == ".DS_Store" :
+                plant_disease_folder_list.remove(disease_folder)
+
+        for plant_disease_folder in plant_disease_folder_list:
+            print(f"[INFO] Processing {plant_disease_folder} ...")
+            plant_disease_image_list = listdir(f"{directory_root}/{plant_folder}/{plant_disease_folder}/")
+                
+            for single_plant_disease_image in plant_disease_image_list :
+                if single_plant_disease_image == ".DS_Store" :
+                    plant_disease_image_list.remove(single_plant_disease_image)
+
+            for image in plant_disease_image_list[:200]:
+                image_directory = f"{directory_root}/{plant_folder}/{plant_disease_folder}/{image}"
+                if image_directory.endswith(".jpg") == True or image_directory.endswith(".JPG") == True:
+                    image_list.append(convert_image_to_array(image_directory))
+                    label_list.append(plant_disease_folder)
+    print("[INFO] Image loading completed")  
+except Exception as e:
+    print(f"Error : {e}")
